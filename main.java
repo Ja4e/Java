27407196b172e42fa50f2cd8698bf8aa9b49a803
Saul Gman <Jakie101@proton.me>
2024-10-09 12:41:41 +0800

    Update main.java

--- main.java
+++ main.java
@@ -6,3 +6,16 @@ public class main{
 // use java 17
 
 https://gist.github.com/skyl/36563a5be809e54dc139
+
+
+cd ~
+
+git clone https://github.com/Homebrew/brew homebrew
+
+mkdir ~/usr/local
+
+# installed packaged directory
+
+echo “export HOMEBREW_PREFIX=~/usr/local” >> ~/.zshrc
+
+echo “export PATH=$PATH:~/homebrew/bin:HOMEBREW_PREFIX/bin” >> ~/.zshrc
