37534f9372e294002787123e86bcdeae71900d48
Ja4e <Jakie101@proton.me>
2024-11-21 00:05:45 +0800

    updated all

--- JavaC2E1.java
+++ JavaC2E1.java
@@ -1,7 +1,11 @@
 public class JavaC2E1{
 	public static void main(String[] args){
 		int enter = IBIO.inputInt("enter: ")
-		
+		if (enter > 60){
+			System.out.println("The number was too big ")
+		}
+		if else (enter < 50){
+			System.out.println("The number was too small")
 		}
 	}
 }
