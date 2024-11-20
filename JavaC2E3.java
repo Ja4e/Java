1cd943f56c29a8eede5a351a9dd47f9417ffd553
Ja4e <Jakie101@proton.me>
2024-11-21 02:08:14 +0800

    mmm updates? mmmm

---
+++ JavaC2E3.java
@@ -0,0 +1,11 @@
+public class JavaC2E2{
+	public static void main(String[] args){
+		int enter = IBIO.inputInt("enter: ");
+		if (enter % 2 == 1){
+			System.out.println("The number is ODD ");
+		}
+		else {
+			System.out.println("The number is EVEN");
+		}
+	}
+}
---
+++ JavaC2E4.java
@@ -0,0 +1,12 @@
+public class JavaC2E4{
+	public static void main(String[] args){
+		for (int i = 0; i < 10; i++){
+			if (i < 10){
+				System.out.println("  " + i);
+			}
+			else{
+				System.out.println(" " + i);
+			}
+		}
+	}
+}
