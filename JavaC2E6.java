14b00fcf5ed1bf0516f328a47d7e7cd45f760d84
Ja4e <Jakie101@proton.me>
2024-11-21 02:54:43 +0800

    mmm updates? mmmm

--- JavaC2E6.java
+++ JavaC2E6.java
@@ -2,11 +2,13 @@ public class JavaC2E6{
 	public static void main(String[] args){
 		for (int i = 0; i < 100; i++){
 			if (i % 2 == 1){
-				if (i < 100){
-					System.out.println("  " + i);
-				}
-				else{
-					System.out.println(" " + i);
+				if (i/3!=0){ //(i % 2 != 0 && i % 3 != 0) will work as well
+					if (i < 100){
+						System.out.println("  " + i);
+					}
+					else{
+						System.out.println(" " + i);
+					}
 				}
 			}
 		}
