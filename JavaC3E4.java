66a2958c2695d02e60cf81139115285ba2e8dc12
Ja4e <Jakie101@proton.me>
2024-11-25 08:39:46 +0800

    mmm updates? mmmm

--- JavaC3E4.java
+++ JavaC3E4.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class JavaC3E4{
-	
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("enter a number: ");
+		int x = scanner.nextInt();
+		do{
+			System.out.print(x+", ");
+			if (x%2==0){
+				x = x/2;
+			}
+			else if (x%2==1){
+				x = (x*3)+1;
+			}
+			if (x == 1){
+				break;
+			}
+		} while (x!=0);
+		System.out.println(x); 
 	}
 }
--- JavaC4E2.java
+++ JavaC4E2.java
@@ -2,29 +2,50 @@ import java.util.Scanner;
 
 public class JavaC4E2{
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);//Couldve used IBIO.inputInt
-		System.out.print("enter first number: ");
-		int a = scanner.nextInt();
-		System.out.print("enter second number: ");
-		int b = scanner.nextInt();
-		int num;
 		do{
-			System.out.println("Press: [1] for addition\n       [2] for multiplication\n       [3] for quit");
-			num = scanner.nextInt();
-			switch (num){
-				case 1:
-					num = a+b;
-					System.out.println("Output: "+num);
-					break;
-				case 2:
-					num = a*b;
-					System.out.println("Output: "+num);
-					break;
-				case 3:
-					System.out.print("quitting...");
-					break;
+			try{
+				while(true){
+					Scanner scanner = new Scanner(System.in);//Couldve used IBIO.inputInt
+					System.out.println("Press: [1] for Continue\n       [2] for quit");
+					System.out.print("Input: ");
+					int num = scanner.nextInt();
+					if (num == 2){
+						System.out.println("Quitting... adios");
+						break;
+					}
+					long d = 0;
+					System.out.print("enter first number: ");
+					long a = scanner.nextInt();
+					System.out.print("enter second number: ");
+					long b = scanner.nextInt();
+					System.out.print("enter third number: ");
+					long c = scanner.nextInt();
+					System.out.print("enter: ");
+/*					int a = scanner.nextInt(); //if you use this code bruh you are taking the laziness to the next level already...
+					for (int d = 0; d<=2; d++){
+					}*/
+					for (long i = 1; i<1000; i++){ //i++ wasnt supposed to be there... what else can i replace this????
+						System.out.print(i + "*" + a + " + ");
+						d=i*a;
+						System.out.print((i+1) + "*" + b + " - ");
+						d=a+(i*b);
+						if (i==999){
+							System.out.print((i+1) + "*" + c +" \n");
+							d=a-(i*c);
+							break;
+						}
+						System.out.print((i+1) + "*" + c + " + ");
+						d=a-(i*c);
+					}
+					System.out.println("output: "+d);
+				}
+			}
+			catch (Exception e){
+				System.out.println("bro all you need is to type in a forking number into the fucking input space");
+				System.out.println("Error {e}");
+				continue;
 			}
 		}
-		while (num != 3);
+		while (true);
 	}
 }
