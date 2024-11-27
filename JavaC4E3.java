d2ad6518f0c68c51c78778f53e2dee7397477fcc
Ja4e <Jakie101@proton.me>
2024-11-27 11:25:41 +0800

    mmm updates? mmmm

--- JavaC4E3.java
+++ JavaC4E3.java
@@ -2,25 +2,39 @@ import java.util.Scanner;
 
 public class JavaC4E3 {
 	public static void main (String[] args) {
-		int sum = 0;
-		int num = 0;
-		int x = 0;
-		
-		for (int i = 1; i <= 1000; i++) {
-			num = i % 3;
-			switch (num) {
-				case 0:
-					x = i * -5;
-					break;
-				case 1:
-					x = i * 7;
-					break;
-				case 2:
-					x = i * 2;
-					break;
+		do{
+			try{
+				Scanner scanner = new Scanner(System.in);
+				System.out.print("Enter a number ");
+				int num = scanner.nextInt();
+				while (num > 1000){
+					System.out.println("Number too big");
+					System.out.print("Enter a number within a range of 1000: ");
+					num = scanner.nextInt();
+				}
+				int o = 0;
+				int i=0, e=0;
+				exit:
+				for ( i =1;  i<=num ; i++){
+					for ( e = 1; e<=num; e++){
+						o = (i*i) + (e*e);
+						if (o == num){ 
+							//System.out.println(i +" " +e);
+							break exit;
+						}
+					}
+				}
+				if (o == num){
+					System.out.println(i + " " + e);
+				}
+				else{
+					System.out.println("impossible");
+				}
 			}
-			sum = sum + x;
+			catch (Exception e){
+				System.out.println("brother check your input");
+			}
 		}
-		System.out.println(sum);
+		while (true);
 	}
 }
