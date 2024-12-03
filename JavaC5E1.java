87400486c27495be267a573a9be16c4c7e47764d
Ja4e <Jakie101@proton.me>
2024-12-03 11:45:37 +0800

    mmm updates? mmmm

--- JavaC5E1.java
+++ JavaC5E1.java
@@ -2,39 +2,21 @@ import java.util.Scanner;
 
 public class JavaC5E1 {
 	public static void main (String[] args) {
-		do{
-			try{
-				Scanner scanner = new Scanner(System.in);
-				System.out.print("Enter a number ");
-				int num = scanner.nextInt();
-				while (num > 1000){
-					System.out.println("Number too big");
-					System.out.print("Enter a number within a range of 1000: ");
-					num = scanner.nextInt();
-				}
-				int o = 0;
-				int i=0, e=0;
-				exit:
-				for ( i =1;  i<=num ; i++){
-					for ( e = 1; e<=num; e++){
-						o = (i*i) + (e*e);
-						if (o == num){ 
-							//System.out.println(i +" " +e);
-							break exit;
-						}
-					}
-				}
-				if (o == num){
-					System.out.println(i + " " + e);
-				}
-				else{
-					System.out.println("impossible");
-				}
-			}
-			catch (Exception e){
-				System.out.println("brother check your input");
-			}
-		}
-		while (true);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("enter first number: ");
+		double a = scanner.nextDouble();
+		System.out.print("enter second number: ");
+		double b = scanner.nextDouble();
+		double num = a/b;
+		System.out.println("division gives: " + num);
 	}
+	
+/*	public static void main2 (String[] args) {
+		double a = IBIO.inputDouble("enter first number: ");
+		double b = IBIO.inputDouble("enter second number: ");
+		double num = a/b;
+		IBIO.output("division gives: " + num);
+	}*/
 }
+
+
