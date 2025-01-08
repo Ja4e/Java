7abedff8a85bc5674218e4891e435f9972bcbbef
ja4e <https://ja4e@github.com>
2025-01-08 13:02:38 +0800

    yes

--- JavaStringDemo.java
+++ JavaStringDemo.java
@@ -1,5 +1,18 @@
 public class JavaStringDemo{
 	public static void main(String args[]){
 		System.out.println("uwu");
+		String m = new String("Jake");
+		System.out.println(m);
+		String q = "lmao this is boring";
+		System.out.println(m==q);
+		System.out.println(m.equals(q));
+		q.substring(2,4);
+		q.equals(m);
+		q.compareTo(m);
+		q.length();
+		System.out.println(q.substring(2,q.length()));
+		System.out.println(q.substring(1,4)); //careful its ranked as 0,1,2,3
+		System.out.println(q.substring(q.length()-2));
+		System.out.println("Java is boring right");
 	}
 }
--- python59.py
+++ python59.py
@@ -1,2 +1,1 @@
-print("uwu")
-print('uwu')
+
