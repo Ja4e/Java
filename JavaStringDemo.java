3af04fdd8b8a19e7bccde25feb3f75faecf18d8a
ja4e <https://ja4e@github.com>
2025-01-09 10:17:15 +0800

    yes

--- JavaStringDemo.java
+++ JavaStringDemo.java
@@ -6,13 +6,14 @@ public class JavaStringDemo{
 		String q = "lmao this is boring";
 		System.out.println(m==q);
 		System.out.println(m.equals(q));
-		q.substring(2,4);
-		q.equals(m);
-		q.compareTo(m);
-		q.length();
+		String a = q.substring(2,4);
+		boolean b = q.equals(m);
+		int c = q.compareTo(m);
+		long d = q.length();
 		System.out.println(q.substring(2,q.length()));
 		System.out.println(q.substring(1,4)); //careful its ranked as 0,1,2,3
 		System.out.println(q.substring(q.length()-2));
 		System.out.println("Java is boring right");
+		System.out.println("b: " + b + ", c: " + c + ", d: " + d);
 	}
 }
