0298ec0c4b7682c2f86d68b80538eb778a0c3e4b
ja4e <https://ja4e@github.com>
2025-01-13 23:08:30 +0800

    yes

---
+++ JavaC9E1.java
@@ -0,0 +1,9 @@
+public class JavaC9E1 {
+	public static void main(String args[]){
+		for (int i = 0; i < 100; i++){
+			double xx = Math.random() * 6;
+			int yy = (int)(xx + 1);
+			System.out.print(yy);
+		}
+	}
+}
