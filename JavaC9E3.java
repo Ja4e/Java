43d7235688dd1a4c1d61ae6c2b6b011c267c79aa
ja4e <https://ja4e@github.com>
2025-02-07 12:30:08 +0800

    yes

--- JavaC9E3.java
+++ JavaC9E3.java
@@ -1,10 +1,10 @@
 public class JavaC9E3 {
 	public static void main(String args[]){
 		int[] num = new int[100];
-			for (int i = 0; i < 100; i++){
-				double xx = Math.random() * 6;
-				num[i] = (int)(xx + 1);
-			}
+		for (int i = 0; i < 100; i++){
+			double xx = Math.random() * 6;
+			num[i] = (int)(xx + 1);
+		}
 		for (int d = 1; d <= 6; d++){
 			for (int i = 0; i < 100; i++){
 				if (num[i]==d){
